9f887c16dc0d56e0d3600fd09c0195a730484f75
최승은 <liberalfilm@gmail.com>
2022-12-19 16:00:57 +0900

    2022-12-19-Mon-16:00

---
+++ src/main/java/co/kr/mono/aop/ui/BluePrinter.java
@@ -0,0 +1,12 @@
+package co.kr.mono.aop.ui;
+
+public class BluePrinter implements Printer{
+    @Override
+    public void print(String msg) {
+
+        System.out.println("----- BluePrinter Start -----");
+        System.out.println(msg);
+        System.out.println("----- BluePrinter End -----");
+
+    }
+}
---
+++ src/main/java/co/kr/mono/aop/ui/MonoPrinter.java
@@ -0,0 +1,12 @@
+package co.kr.mono.aop.ui;
+
+public class MonoPrinter implements Printer{
+    @Override
+    public void print(String msg) {
+
+        System.out.println("----- MonoPrinter Start -----");
+        System.out.println(msg);
+        System.out.println("----- MonoPrinter End -----");
+
+    }
+}
